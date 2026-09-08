6fe24101c5db20786feeac78d3887ccc9c32c8d2
yungshadow213-prog <yungshadow213@gmail.com>
2026-09-08 09:59:51 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+const CACHE='game-night-pwa-v3';
+const SHELL=['/','/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('/'))))});
